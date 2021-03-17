7b7828ae7a092d3aebc93b586615b38d17ad170a
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-03-17 21:37:30 +0100

    one

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto start
+echo.
+echo RUNCRUD.BAT has errors - breaking work
+goto fail
+
+:fail
+echo.
+echo There were errors
+goto end
+
+
+
+:start
+start "" http://localhost:8080/crud/v1/task/getTasks
+echo.
+echo Opening browser
+
+:end
