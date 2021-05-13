feaed6e4b2aa0fe0f6e7b7eabc29db0d5f94cfd8
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-05-13 19:58:00 +0200

    22.4 EmptyList test

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,21 +1,16 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrelloCardDto {
 
     private String name;
     private String description;
     private String pos;
     private String listId;
-
-    //poniższe usunąć do 22.3
-
-    public TrelloCardDto(String name, String description, String pos, String listId) {
-        this.name = name;
-        this.description = description;
-        this.pos = pos;
-        this.listId = listId;
-    }
 }
