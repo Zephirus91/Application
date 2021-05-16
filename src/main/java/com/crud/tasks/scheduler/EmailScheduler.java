4082ea8b4f3470d7fa4da09e182ee2aa91b3fc6e
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-05-16 20:07:27 +0200

    23.4 Schedule update

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -21,10 +21,7 @@ public class EmailScheduler {
     @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
-        String message = "Currently in database you got: " + size + " task.";
-        if (size > 1) {
-            message = "Currently in database you got: " + size + " task.";
-        }
+        String message = ("Currently in database you got: " + size + (size > 1 ? " tasks" : " task"));
         simpleEmailService.send(
                 new Mail(
                         adminConfig.getAdminMail(),
