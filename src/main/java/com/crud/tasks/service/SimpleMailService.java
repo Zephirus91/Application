87b39d818e75ac8a0be27cd2adf9c160fd68619f
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-05-21 18:21:40 +0200

    32.2 Creating mail

---
+++ src/main/java/com/crud/tasks/service/SimpleMailService.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+public class SimpleMailService {
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
+        return mailMessage;
+    }
+}
